ba24c0a787ac8d6adb8a0ac85ac9e0d7d1734e6e
Dee Lynn <deannalguzman@gmail.com>
2019-10-17 20:37:36 -0700

    added the async function in app.js and added all of the role questions

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-const Employee = require("./lib/Employee");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const ManagerQuest = require("./lib/ManagerQuest");
+const InternQuest = require("./lib/InternQuest");
+const EngineerQuest = require("./lib/EngineerQuest")
+
+
+async function questionsEm() {
+    var employees = []
+    let managerAns = await ManagerQuest();
+    // employees.push(new ManagerQuest(1, managerAns.name));
+    let i = 0;
+    while (i < parseInt(managerAns.numEmployees)) {
+        let employeetype = await inquirer.prompt([
+            {
+            type: "list",
+            name: "role",
+            message: "What is the employee role",
+            choices: ["Engineer", "Intern"]
+        }
+    ])
+        console.log(employeetype)
+        switch (employeetype.Role) {
+            case "Intern":
+                employeeAns = await InternQuest(i);
+                employees.push(new Intern(i+2, employeeAns.name, employeeAns.email, employeeAns.school));
+                break;
+            case "Engineer":
+                employeeAns = await EngineerQuest(i);
+                employees.push(new Engineer(i+2, employeeAns.name, employeeAns.email, employeeAns.github));
+                break;
+        }
+        i++;
+    }
+}
+questionsEm();
+
